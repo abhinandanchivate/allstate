faa31be25de49f5574ffce4ba52acd9f9af317e7
abhin <abhinandanchivate2000@gmail.com>
2020-11-20 17:18:35 +0530

    Exception handler

--- demo/src/main/java/com/allstate/restdemo/exception/ResouceNotFoundException.java
+++ demo/src/main/java/com/allstate/restdemo/exception/ResouceNotFoundException.java
@@ -1,5 +1,9 @@
 package com.allstate.restdemo.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResouceNotFoundException extends Exception {
 
 	public ResouceNotFoundException(String msg) {
