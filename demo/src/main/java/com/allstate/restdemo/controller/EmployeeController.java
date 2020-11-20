7fb3f42d984fa0c77236c3c9e0880b21f742b922
abhin <abhinandanchivate2000@gmail.com>
2020-11-20 18:03:01 +0530

    controller

--- demo/src/main/java/com/allstate/restdemo/controller/EmployeeController.java
+++ demo/src/main/java/com/allstate/restdemo/controller/EmployeeController.java
@@ -3,11 +3,19 @@ package com.allstate.restdemo.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.allstate.restdemo.exception.ResouceNotFoundException;
 import com.allstate.restdemo.model.Employee;
@@ -32,7 +40,19 @@ public class EmployeeController {
 	public ResponseEntity<Employee> getEmployeeById(@PathVariable(value="id") long empId) throws ResouceNotFoundException {
 		Employee employee = employeeRepository.findById(empId).orElseThrow(()->new ResouceNotFoundException("Data not found"));
 		
-		return null;
+		return ResponseEntity.ok().body(employee);
 		
+	}
+	
+	@PostMapping("/employees")
+	public ResponseEntity<Void> createEmployee(@Valid @RequestBody Employee employee , UriComponentsBuilder uriComponentsBuilder, HttpServletRequest httpServletRequest ) {
+		
+		Employee employee2 = employeeRepository.save(employee);
+		
+		System.out.println(httpServletRequest.getRequestURI());
+		
+		UriComponents uriComponents = uriComponentsBuilder.path(httpServletRequest.getRequestURL()+"/{id}")
+				.buildAndExpand(employee2.getId());
+		return ResponseEntity.created(uriComponents.toUri()).build();
 	}
 }
